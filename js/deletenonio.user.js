ec9b0196cacec0ea1b6a6bd1c084076cba33f724
Serpa <dippas@users.noreply.github.com>
2019-06-12 00:29:20 +0000

    UPD added missing sites, FIX overflow overriden sometimes

--- js/deletenonio.user.js
+++ js/deletenonio.user.js
@@ -4,17 +4,17 @@
 // @namespace    https://github.com/dippas/DeleteNonio/
 // @homepage     https://github.com/dippas/DeleteNonio/
 // @description  Delete annoying popup for nonio registration - Privacy issues
-// @version      0.0.2
+// @version      0.1.0
 // @supportURL   https://github.com/dippas/DeleteNonio/issues
-// @updateURL    https://raw.githuabusercontent.com/dippas/DeleteNonio/master/js/deletenonio.user.js
-// @downloadURL  https://raw.githubusercontent.com/dippas/DeleteNonio/master/js/deletenonio.user.js
+// @updateURL    https://raw.githuabusercontent.com/dippas/DeleteNonio/master/js/deletenonio.min.user.js
+// @downloadURL  https://raw.githubusercontent.com/dippas/DeleteNonio/master/js/deletenonio.min.user.js
+// @match        https://*.aquelamaquina.pt/*
 // @match        https://*.xl.pt/*
 // @match        https://*.publico.pt/*
 // @match        https://*.sapo.pt/*
 // @match        https://*.blitz.pt/*
 // @match        https://*.visao.pt/*
 // @match        https://*.expressoemprego.pt/*
-// @match        https://*.exameinformatica.pt/*
 // @match        https://*.cmjornal.pt/*
 // @match        https://*.record.pt/*
 // @match        https://*.jornaldenegocios.pt/*
@@ -37,19 +37,21 @@ const deleteNonio = {
 		html: document.documentElement,
 		body: document.body
 	},
-
 	events() {
-		this.el.html.style.overflow = 'auto';
-		this.el.body.style.overflow = 'auto';
-      	document.querySelectorAll('[id^="layer_gatting"]')[0].outerHTML = '';
-        document.querySelectorAll('.nonioBox')[0].outerHTML = '';
+		this.el.html.style = 'overflow: auto !important';
+		this.el.body.style = 'overflow: auto !important';
+		document.querySelectorAll('#imp-content-gate-root')[0] ? document.querySelectorAll('#imp-content-gate-root')[0].outerHTML = '' : '';
+		document.querySelectorAll('[id^="layer_gatting"]')[0] ? document.querySelectorAll('[id^="layer_gatting"]')[0].outerHTML = '' : '';
+		document.querySelectorAll('.nonioBox')[0] ? document.querySelectorAll('.nonioBox')[0].outerHTML = '' : '';
+		document.querySelectorAll('.warning-nonio-overlay')[0] ? document.querySelectorAll('.warning-nonio-overlay')[0].outerHTML = '' : '';
+		document.querySelectorAll('.tp-modal')[0] ? document.querySelectorAll('.tp-modal')[0].outerHTML = '' : '';
+		document.querySelectorAll('.tp-backdrop')[0] ? document.querySelectorAll('.tp-backdrop')[0].outerHTML = '' : '';
+		document.querySelectorAll('.tp-iframe-wrapper')[0] ? document.querySelectorAll('.tp-iframe-wrapper')[0].outerHTML = '' : '';
 	},
-
 	init() {
 		window.onload = () => {
-          this.events();
+			this.events()
 		}
 	}
-}
-
+};
 deleteNonio.init();
