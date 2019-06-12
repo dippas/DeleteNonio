e6ec47c6d1dc8543173973ad9fda8543c83f4349
Serpa <dippas@users.noreply.github.com>
2019-06-12 00:31:19 +0000

    UPD minified file from previous commit

--- js/deletenonio.min.user.js
+++ js/deletenonio.min.user.js
@@ -30,6 +30,4 @@
 // @grant        none
 // ==/UserScript==
 
-
-
-const deleteNonio={el:{html:document.documentElement,body:document.body},events(){this.el.html.style.overflow="auto",this.el.body.style.overflow="auto",document.querySelectorAll("[id^=\"layer_gatting\"]")[0].outerHTML="",document.querySelectorAll(".nonioBox")[0].outerHTML=""},init(){window.onload=()=>{this.events()}}};deleteNonio.init();
+const deleteNonio={el:{html:document.documentElement,body:document.body},events(){this.el.html.style="overflow: auto !important",this.el.body.style="overflow: auto !important",document.querySelectorAll("#imp-content-gate-root")[0]?document.querySelectorAll("#imp-content-gate-root")[0].outerHTML="":"",document.querySelectorAll("[id^=\"layer_gatting\"]")[0]?document.querySelectorAll("[id^=\"layer_gatting\"]")[0].outerHTML="":"",document.querySelectorAll(".nonioBox")[0]?document.querySelectorAll(".nonioBox")[0].outerHTML="":"",document.querySelectorAll(".warning-nonio-overlay")[0]?document.querySelectorAll(".warning-nonio-overlay")[0].outerHTML="":"",document.querySelectorAll(".tp-modal")[0]?document.querySelectorAll(".tp-modal")[0].outerHTML="":"",document.querySelectorAll(".tp-backdrop")[0]?document.querySelectorAll(".tp-backdrop")[0].outerHTML="":"",document.querySelectorAll(".tp-iframe-wrapper")[0]?document.querySelectorAll(".tp-iframe-wrapper")[0].outerHTML="":""},init(){window.onload=()=>{this.events()}}};deleteNonio.init();
