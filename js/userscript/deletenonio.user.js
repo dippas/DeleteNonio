5ff90d96339ba0c83a6679e082830b41d1cfa2b3
Serpa <serpa14@gmail.com>
2020-04-17 15:50:00 +0100

    FIX typo in url buyme popup

--- js/userscript/deletenonio.user.js
+++ js/userscript/deletenonio.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/dippas/DeleteNonio/
 // @homepage     https://github.com/dippas/DeleteNonio/
 // @description  Remover NONIO. Sempre actualizado para remover o nonio dos sites. Disponível como script e Extensão para Firefox e Chrome.
-// @version      1.4.1
+// @version      1.4.2
 // @supportURL   https://github.com/dippas/DeleteNonio/issues
 // @downloadURL  https://raw.githubusercontent.com/dippas/DeleteNonio/master/js/userscript/deletenonio.user.js
 // @match        https://*.aquelamaquina.pt/*
