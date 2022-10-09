45e125bb92b4beb6539072563c27ebfc8023f749
dippas <dippas.dev@gmail.com>
2022-10-09 22:39:34 +0100

    tidy up

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Popup from './components/Popup/Popup'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Popup from './components/Popup/Popup';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Popup />
   </React.StrictMode>
-)
+);
